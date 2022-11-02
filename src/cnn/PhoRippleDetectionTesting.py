4c8d782a96111529313267a32efdb3b96acaf7f7
Pho Hale <CommanderPho@users.noreply.github.com>
2022-11-02 18:58:46 -0400

    renamed _run_batch_cycle to _run_single_shank_computation

--- src/cnn/PhoRippleDetectionTesting.py
+++ src/cnn/PhoRippleDetectionTesting.py
@@ -44,25 +44,25 @@ class ExtendedRippleDetection(object):
             srLfp = 1250, downsampled_fs = 1250, 
             overlapping = True, window_size = 0.0128, window_stride = 0.0064, # window parameters
             ripple_detection_threshold=0.7,
-            active_shank_channels_lists = [[72,73,74,75,76,77,78,79], [81,82,83,84,85,86,87,88]]
+            active_shank_channels_lists = [[72,73,74,75,76,77,78,79], [81,82,83,84,85,86,87,88]],
+            **kwargs
             ):
         self.active_session_folder = active_session_folder
         self.loaded_eeg_data, self.active_session_eeg_data_filepath, self.active_session_folder = self.load_eeg_data(active_session_folder=active_session_folder, numchannel=numchannel)
-        out_all_ripple_results = self.compute_ripples(self.model, self.loaded_eeg_data, srLfp=srLfp, downsampled_fs=downsampled_fs, overlapping=overlapping, window_size=window_size, window_stride=window_stride, ripple_detection_threshold=ripple_detection_threshold, active_shank_channels_lists=active_shank_channels_lists, out_all_ripple_results=None)
-        self.out_all_ripple_results = out_all_ripple_results
+        self.out_all_ripple_results = self.compute_ripples(self.model, self.loaded_eeg_data, srLfp=srLfp, downsampled_fs=downsampled_fs, overlapping=overlapping, window_size=window_size, window_stride=window_stride, ripple_detection_threshold=ripple_detection_threshold, active_shank_channels_lists=active_shank_channels_lists, out_all_ripple_results=None, **(dict(debug_trace_computations_output=True, debug_print=False)|kwargs))
 
         out_all_ripple_results_filepath = active_session_folder.joinpath('out_all_ripple_results.pkl')
         with open(out_all_ripple_results_filepath, 'wb') as f:
             print(f'saving results to {str(out_all_ripple_results_filepath)}...')
-            pickle.dump(out_all_ripple_results, f)
+            pickle.dump(self.out_all_ripple_results, f)
         print(f'done.')
-        flattened_pred_ripple_start_stop_times = np.vstack([a_result['pred_times'] for a_result in out_all_ripple_results['results'].values() if np.size(a_result['pred_times'])>0])
+        flattened_pred_ripple_start_stop_times = np.vstack([a_result['pred_times'] for a_result in self.out_all_ripple_results['results'].values() if np.size(a_result['pred_times'])>0])
         print(f'flattened_pred_ripple_start_stop_times: {np.shape(flattened_pred_ripple_start_stop_times)}') # (6498, 2)
         ripple_df = pd.DataFrame({'start':flattened_pred_ripple_start_stop_times[:,0], 'stop': flattened_pred_ripple_start_stop_times[:,1]})
         self.ripple_df = ripple_df
         print(f'Saving ripple_df to csv: {self.predicted_ripples_dataframe_save_filepath}')
         ripple_df.to_csv(self.predicted_ripples_dataframe_save_filepath)
-        return ripple_df, out_all_ripple_results
+        return ripple_df, self.out_all_ripple_results
 
     @property
     def predicted_ripples_dataframe_save_filepath(self):
@@ -160,6 +160,7 @@ class ExtendedRippleDetection(object):
     def _downsample_data(data, fs, downsampled_fs):
         # Dowsampling
         if fs > downsampled_fs:
+            print("Downsampling data from %d Hz to %d Hz..."%(fs, downsampled_fs), end=" ")
             downsampled_pts = np.linspace(0, data.shape[0]-1, int(np.round(data.shape[0]/fs*downsampled_fs))).astype(int)
             downsampled_data = data[downsampled_pts, :]
 
@@ -168,7 +169,7 @@ class ExtendedRippleDetection(object):
             print("Original sampling rate below 1250 Hz!")
             return None
         else:
-            print("Original sampling rate equals 1250 Hz!")
+            # print("Original sampling rate equals 1250 Hz!")
             downsampled_data = data
 
         # Change from int16 to float16 if necessary
@@ -198,10 +199,8 @@ class ExtendedRippleDetection(object):
         return data
 
     @classmethod
-    def _run_batch_cycle(cls, model, loaded_eeg_data, active_shank, active_shank_channels, srLfp, downsampled_fs, overlapping, window_size, window_stride, ripple_detection_threshold):
-        """ captures:
-        srLfp, downsampled_fs
-        overlapping, window_size, window_stride
+    def _run_single_shank_computation(cls, model, loaded_eeg_data, active_shank, active_shank_channels, srLfp, downsampled_fs, overlapping, window_size, window_stride, ripple_detection_threshold, debug_trace_computations_output=False, debug_print=False):
+        """ Runs a single set of 8 channels (from one 8-channel probe)
         """
         ## Begin:
         if isinstance(active_shank_channels, list):
@@ -211,13 +210,14 @@ class ExtendedRippleDetection(object):
         active_shank_channels = active_shank_channels - 1
 
         fs = srLfp
-        loaded_data = loaded_eeg_data[:,active_shank_channels]
 
+        # Get the subset of the data corresponding to only the active channels 
+        loaded_data = loaded_eeg_data[:,active_shank_channels]
+        if debug_print:
         print("Shape of loaded data: ", np.shape(loaded_data))
-        # Downsample data
-        
-        print("Downsampling data from %d Hz to %d Hz..."%(fs, downsampled_fs), end=" ")
+        # Downsample data (if needed)
         data = cls._downsample_data(loaded_data, fs, downsampled_fs)
+        if debug_print:
         print("Done!")
 
         # Normalize it with z-score
@@ -240,15 +240,17 @@ class ExtendedRippleDetection(object):
         predictions = model.predict(X, verbose=True)
         print("Done!")
 
-        # This threshold can be changed
-        
         print("Getting detected ripples indexes and times...", end=" ")
         pred_indexes = get_predictions_indexes(data, predictions, window_size=window_size, stride=window_stride, fs=downsampled_fs, threshold=ripple_detection_threshold)
         pred_times = pred_indexes / downsampled_fs
         print("Done!")
 
         ## Single result output
-        return {'shank':active_shank, 'channels': active_shank_channels, 'predictions': predictions, 'pred_indexes': pred_indexes, 'pred_times': pred_times}
+        curr_shank_computations = {'shank':active_shank, 'channels': active_shank_channels, 'time_windows': X, 'predictions': predictions, 'pred_indexes': pred_indexes, 'pred_times': pred_times}
+        if debug_trace_computations_output:
+            curr_shank_computations['data'] = data
+
+        return curr_shank_computations
 
 
     ## Batch
@@ -272,7 +274,7 @@ class ExtendedRippleDetection(object):
     def compute_ripples(cls, model, loaded_eeg_data, active_shank_channels_lists, 
         srLfp = 1250, downsampled_fs = 1250, 
         overlapping = True, window_size = 0.0128, window_stride = 0.0064, # window parameters
-        ripple_detection_threshold=0.7, out_all_ripple_results=None):
+        ripple_detection_threshold=0.7, debug_trace_computations_output=False, out_all_ripple_results=None, debug_print=False):
         
         ## Create Empty Output:
         computation_params = None
@@ -292,7 +294,9 @@ class ExtendedRippleDetection(object):
         for active_shank, active_shank_channels in enumerate(active_shank_channels_lists):
             print(f'working on shank {active_shank} with channels: {active_shank_channels}...')
             try:
-                out_result = cls._run_batch_cycle(model, loaded_eeg_data, active_shank, active_shank_channels, srLfp=srLfp, downsampled_fs=downsampled_fs, overlapping=overlapping, window_size=window_size, window_stride=window_stride, ripple_detection_threshold=ripple_detection_threshold)
+                out_result = cls._run_single_shank_computation(model, downsampled_loaded_eeg_data, active_shank, active_shank_channels, srLfp=post_downsampling_srLfp, downsampled_fs=downsampled_fs,
+                    overlapping=overlapping, window_size=window_size, window_stride=window_stride, ripple_detection_threshold=ripple_detection_threshold,
+                    debug_trace_computations_output=debug_trace_computations_output, debug_print=debug_print)
                 out_all_ripple_results['results'][active_shank] = out_result
             except ValueError as e:
                 out_result = {} # empty output result
@@ -301,6 +305,10 @@ class ExtendedRippleDetection(object):
         print(f'done with all!')
         return out_all_ripple_results
 
+
+# ==================================================================================================================== #
+# Start MAIN                                                                                                           #
+# ==================================================================================================================== #
 
 def main_compute_with_params_loaded_from_xml(local_session_path, **kwargs):
     """Loads the session recording info from the XML located in the local_session_path (session folder), and the computes the ripples from that data
@@ -312,11 +320,12 @@ def main_compute_with_params_loaded_from_xml(local_session_path, **kwargs):
         _type_: _description_
 
     Usage:
+        from src.cnn.PhoRippleDetectionTesting import ExtendedRippleDetection, main_compute_with_params_loaded_from_xml
 
         # local_session_path = Path(r'W:\Data\KDIBA\gor01\one\2006-6-08_14-26-15')
         # local_session_path = Path(r'W:\Data\KDIBA\gor01\one\2006-6-08_14-26-15')
         local_session_path = Path(r'W:\Data\KDIBA\gor01\one\2006-6-13_14-42-6')
-        ripple_df, out_all_ripple_results, out_all_ripple_results = main_compute_with_params_loaded_from_xml(active_local_session_path)
+        ripple_df, out_all_ripple_results, out_all_ripple_results = main_compute_with_params_loaded_from_xml(local_session_path)
 
 
     """
@@ -331,14 +340,11 @@ def main_compute_with_params_loaded_from_xml(local_session_path, **kwargs):
          'overlapping': True, 'window_size': 0.0128, 'window_stride': 0.0064} | kwargs))
 
     # out_all_ripple_results
-    ripple_df.to_pickle(active_local_session_path.joinpath('ripple_df.pkl'))
+    ripple_df.to_pickle(local_session_path.joinpath('ripple_df.pkl'))
     print(f'done. Exiting.')
-    return ripple_df, out_all_ripple_results, out_all_ripple_results
+    return test_detector, ripple_df, out_all_ripple_results, out_all_ripple_results
 
 
-# ==================================================================================================================== #
-# Start MAIN                                                                                                           #
-# ==================================================================================================================== #
 if __name__ == '__main__':
     # model_path = r'C:\Users\pho\repos\cnn-ripple\model'
     # g_drive_session_path = Path('/content/drive/Shareddrives/Diba Lab Data/KDIBA/gor01/one/2006-6-08_14-26-15')
@@ -348,7 +354,7 @@ if __name__ == '__main__':
     local_session_paths_list = [local_session_parent_path.joinpath(a_name).resolve() for a_name in local_session_names_list]
 
     active_local_session_path: Path = local_session_paths_list[0]
-    ripple_df, out_all_ripple_results, out_all_ripple_results = main_compute_with_params_loaded_from_xml(active_local_session_path)
+    test_detector, ripple_df, out_all_ripple_results, out_all_ripple_results = main_compute_with_params_loaded_from_xml(active_local_session_path)
 
 
     # # active_shank_channels_lists = [a_list[:8] for a_list in active_shank_channels_lists if len(a_list)>=8]
